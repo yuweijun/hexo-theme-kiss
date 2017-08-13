9810e88814357b215e0a692346edf4aa682ae94a
yuweijun <yuweijun@live.com>
2017-08-13 17:35:19 +0800

    修改bash输出内容的显示样式

--- scripts/fa-usd.js
+++ scripts/fa-usd.js
@@ -1,5 +1,6 @@
 hexo.extend.filter.register('after_post_render', function(data){
     data.content = data.content.replace(/(\$&gt;\s*)([\w-]+)/g, '<i class="fa fa-usd"></i> <span class="built_in">$2</span>')
-                               .replace(/(\$&gt;)(\s?)/g, '<i class="fa fa-usd"></i>$2');
+                               .replace(/(\$&gt;)(\s?)/g, '<i class="fa fa-usd"></i>$2')
+                               .replace(/<div class="line">(&gt;\s?)(.*?<\/div>)/g, '<div class="line output">$2');
     return data;
 });
