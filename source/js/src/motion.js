4a152860765add3cd634fa1c9272a8cded0012b6
yuweijun <yuweijun@live.com>
2017-09-21 21:18:38 +0800

    update code high light theme

--- source/js/src/motion.js
+++ source/js/src/motion.js
@@ -190,7 +190,6 @@ $(document).ready(function () {
       var $brand = $('.brand');
       var $title = $('.site-title');
       var $subtitle = $('.site-subtitle');
-      var $logoLineTop = $('.logo-line-before i');
       var $logoLineBottom = $('.logo-line-after i');
 
       $brand.size() > 0 && sequence.push({
@@ -198,12 +197,6 @@ $(document).ready(function () {
         p: {opacity: 1},
         o: {duration: 200}
       });
-
-      NexT.utils.isMist() && hasElement([$logoLineTop, $logoLineBottom]) &&
-      sequence.push(
-        getMistLineSettings($logoLineTop, '100%'),
-        getMistLineSettings($logoLineBottom, '-100%')
-      );
 
       hasElement($title) && sequence.push({
         e: $title,
