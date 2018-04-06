0921f47037c62f003097afbd94f4fdc6f3920d8c
yuweijun <yuweijun@live.com>
2018-04-06 14:26:40 +0800

    fix highlight line output using hexo filter after_render:html

---
+++ scripts/fix-highlight-js-output.js
@@ -0,0 +1,36 @@
+/**
+ * 将命令输出的结果加上classname: output，以区别bash中的命令
+ * 删除</span>标签后面多余的<br>标签
+ */
+hexo.extend.filter.register('after_post_render', function(data){
+    console.log("after post render :", data.source);
+    data.content = data.content.replace(/class="line">&gt;\s?/g, 'class="line output">');
+    return data;
+});
+hexo.extend.filter.register('before_post_render', function(data){
+    console.log("before post render :", data.source);
+    data.title = data.title.toLowerCase();
+    return data;
+});
+
+hexo.extend.filter.register('after_render:html', function(str, data){
+    console.log("after render html :", data.path);
+    return str.replace(/<\/span><br>/g, '</span>').replace(/class="line">&gt;\s?/g, 'class="line output">');
+});
+
+hexo.extend.filter.register('before_exit', function(){
+    console.log("before exist");
+});
+
+hexo.extend.filter.register('before_generate', function(){
+    console.log("before generate");
+});
+
+hexo.extend.filter.register('after_generate', function(){
+    console.log("after generate");
+});
+
+hexo.extend.filter.register('after_exit', function(){
+    console.log("after exist");
+});
+
